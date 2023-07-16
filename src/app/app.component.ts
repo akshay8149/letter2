e4470503f1ef5eff82bda0e10c848e59543daf0e
akshay <akshay147@yahoo.in>
2023-07-16 14:08:13 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import translate from "translate";
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'letter';
+  
+  title = 'shreeComp';
+  name ="";
+  age: string = '';
+  occ:string="";
+  address:string="";
+  showLetter: boolean = false;
+
+  engName: string = '';
+  engOcc: string = '';
+  engAddress: string = '';
+  engAge: string = '';
+
+  async print() {
+    if(this.name && this.age && this.address && this.occ){
+      this.showLetter = true;
+      this.engName =  await translate(this.name, {from:'Marathi', to: 'en'});
+      this.engAddress =  await translate(this.address, {from:'Marathi', to: 'en'});
+      this.engAge =  await translate(this.age, {from:'Marathi', to: 'en'});
+      this.engOcc =  await translate(this.occ, {from:'Marathi', to: 'en'});
+     }
+  }
+
+  onPrint() {
+    const printContents:any = document.getElementById('letter')?.innerHTML;
+     const originalContents = document.body.innerHTML;
+     document.body.innerHTML = printContents;
+     window.print();
+     document.body.innerHTML = originalContents;
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxBarcode6Module } from 'ngx-barcode6';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    NgxBarcode6Module
   ],
   providers: [],
   bootstrap: [AppComponent]
